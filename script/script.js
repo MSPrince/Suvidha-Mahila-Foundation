cbdc664dc425e6dd1658fc316a1c3229c4bdea94
M S Prince <135182786+MSPrince@users.noreply.github.com>
2024-01-14 22:53:37 +0530

    complete Code

--- script/script.js
+++ script/script.js
@@ -1,36 +1,15 @@
-// JavaScript to handle the window scroll event
-window.onscroll = function () {
-  if (
-    document.body.scrollTop > 100 ||
-    document.documentElement.scrollTop > 100
-  ) {
-    document.body.classList.add("scrolled");
-    document.querySelector(".navbar").classList.add("scrolled");
-  } else {
-    document.body.classList.remove("scrolled");
-    document.querySelector(".navbar").classList.remove("scrolled");
-  }
-};
 
 // ========================================================
-document
-  .getElementById("contactForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-    // Assume successful form submission for demonstration
-    // In a real-world scenario, you would handle the form submission asynchronously
-    // (e.g., using AJAX) and display the thank you message accordingly
-    showThankYouMessage();
+
+
+
+// ==================================gallery script================================================================
+$(document).ready(function () {
+  $('#viewAllButton').on('click', function () {
+      $('.image-button.hidden').removeClass('hidden');
+      $(this).hide();
   });
+});
 
-function showThankYouMessage() {
-  // Hide the form
-  document.getElementById("contactForm").style.display = "none";
-  // Show the thank you message
-  document.getElementById("thankYouMessage").style.display = "block";
-  // Close the modal after a delay (3 seconds in this example)
-  setTimeout(function () {
-    $("#contactModal").modal("hide");
-  }, 3000);
-}
-// ========================================================
+// ==================================gallery script================================================================
+
