6873f9314325e264f295f286452374c734690152
M S Prince <135182786+MSPrince@users.noreply.github.com>
2024-01-15 09:31:12 +0530

    complete code

--- script/script.js
+++ script/script.js
@@ -13,3 +13,24 @@ $(document).ready(function () {
 
 // ==================================gallery script================================================================
 
+
+
+
+
+
+
+document.addEventListener(
+  "contextmenu",
+  (e) => {
+    e.preventDefault();
+  },
+  false
+);
+
+
+document.addEventListener("keydown", (e) => {
+  if (e.ctrlKey || e.keyCode == 123) {
+    e.stopPropagation();
+    e.preventDefault();
+  }
+});
